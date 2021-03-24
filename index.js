234217ecd374739347c5b1ec307026c1184c3c40
ppidcard <tvxqlikei@live.cn>
2021-03-24 21:51:52 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-function shout(string) {
-    return string.toUpperCase();
-  };
-function whisper(string) {
-    return string.toLowerCase();
+function shout(string){
+  return string.toUpperCase();
 }
-function logShout (string) {
-    console.log(string.toUpperCase( ));
+console.log(shout("hello"));
+
+function whisper(string){
+  return string.toLowerCase();
+}
+console.log(whisper("HELLO"));
+
+function logShout(string) {
+ console.log(string.toUpperCase());
+}
+logShout("hello");
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+logWhisper("HELLO");
+
+function sayHiToGrandma(string) {
+  if (string===string.toLowerCase()) {
+    return "I can't hear you!";
   }
-  function logWhisper (string) {
-    console.log(string.toLowerCase( ));
+  else if (string==="I love you, Grandma.") {
+    return "I love you, too.";
   }
-  function sayHiToGrandma (string) {
-    var cantUnswer = "I can't hear you!";
-    var yesUnswer = "YES INDEED!";
-    var lovUnswer = "I love you, too.";
-    if (string.toLowerCase(string) === string) {
-      return cantUnswer;
-    }
-    else if (string.toUpperCase(string) === string) {
-      return yesUnswer;
-    }
-    else if ("I love you, Grandma." === string) {
-      return lovUnswer
-    }
-  }
+  else if (string===string.toUpperCase()){
+    return "YES INDEED!";
+}
+}
+
+
